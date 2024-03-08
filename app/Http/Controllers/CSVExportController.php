3d9a2c8230454ef01da7fa6978cd0b66f13f155e
Proge0 <jonavilla321@gmail.com>
2024-03-08 15:33:23 -0300

    comentarios

--- app/Http/Controllers/CSVExportController.php
+++ app/Http/Controllers/CSVExportController.php
@@ -14,15 +14,19 @@ class CSVExportController extends Controller
     {
         $anexos = Anexos::all();
 
+        // Crea una instancia de la clase Spreadsheet
         $spreadsheet = new Spreadsheet();
+        // Obtiene la hoja de cálculo activa
         $sheet = $spreadsheet->getActiveSheet();
-
+        // Establece los encabezados de las columnas en la primera fila de la hoja de cálculo
         $sheet->setCellValue('A1', 'Número Público');
         $sheet->setCellValue('B1', 'Número de Anexo');
         $sheet->setCellValue('C1', 'Nombre de Anexo');
         $sheet->setCellValue('D1', 'Departamento');
 
         $row = 2;
+
+        // Itera sobre los registros de Anexos y llena las celdas correspondientes en la hoja de cálculo
         foreach ($anexos as $anexo) {
             $sheet->setCellValue('A' . $row, $anexo->numeros_publicos);
             $sheet->setCellValue('B' . $row, $anexo->anexo);
@@ -30,10 +34,13 @@ class CSVExportController extends Controller
             $sheet->setCellValue('D' . $row, $anexo->departamento);
             $row++;
         }
-
+        // Crea una instancia de la clase Xlsx para escribir en el formato de archivo XLSX
         $writer = new Xlsx($spreadsheet);
+
+        // Define el nombre del archivo con la fecha actual para evitar sobrescribir archivos existentes
         $filename = 'anexos' . date('Y-m-d') . '.xlsx';
 
+        // Configura las cabeceras HTTP para indicar que se trata de un archivo Excel descargable
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment; filename="' . $filename . '"');
         header('Cache-Control: max-age=0');
--- app/Http/Controllers/CSVImportController.php
+++ app/Http/Controllers/CSVImportController.php
@@ -7,41 +7,48 @@ use PhpOffice\PhpSpreadsheet\Reader\Csv;
 use App\Models\Anexos;
 class CSVImportController extends Controller
 {
-public function import(Request $request)
-    {
-        $request->validate([
-            'file' => 'required|mimes:csv,txt,xlsx',
-        ]);
 
-        $file = $request->file('file');
-        $filepath = $file->getRealPath();
+    // Método para importar datos desde un archivo CSV a la base de datos
+    public function import(Request $request)
+        {
+            // Validación del archivo de entrada, asegurando que sea de tipo csv, txt o xlsx
+            $request->validate([
+                'file' => 'required|mimes:csv,txt,xlsx',
+            ]);
+            // Obtiene el archivo del formulario y la ruta real del archivo
+            $file = $request->file('file');
+            $filepath = $file->getRealPath();
 
-        $reader = new Csv();
-        $spreadsheet = $reader->load($filepath);
-        $sheet = $spreadsheet->getActiveSheet();
-        $highestRow = $sheet->getHighestRow();
-        $anexos = [];
+            // Crea una instancia de la clase Csv para leer el archivo, carga el archivo en un objeto spreadsheet, obtiene una hoja de calculo activa
+            // obtiene el número de la hoja más alto y inicializa un arreglo para almacenar los datos en la tabla Anexos
+            $reader = new Csv();
+            $spreadsheet = $reader->load($filepath);
+            $sheet = $spreadsheet->getActiveSheet();
+            $highestRow = $sheet->getHighestRow();
+            $anexos = [];
 
-        for ($row = 2; $row <= $highestRow; $row++) {
-                $numero_publico = $sheet->getCell('A'.$row)->getValue();
-                $anexo = $sheet->getCell('B'.$row)->getValue();
-                $nombre_anexo = $sheet->getCell('C'.$row)->getValue();
-                $departamento = $sheet->getCell('D'.$row)->getValue();
+            // Itera sobre las filas a partir de la segunda, ya que la primera suele contener encabezados
+            for ($row = 2; $row <= $highestRow; $row++) {
+                    $numero_publico = $sheet->getCell('A'.$row)->getValue();
+                    $anexo = $sheet->getCell('B'.$row)->getValue();
+                    $nombre_anexo = $sheet->getCell('C'.$row)->getValue();
+                    $departamento = $sheet->getCell('D'.$row)->getValue();
+                
+                // Busca si ya existe un Anexo con el mismo número de anexo en la base de datos
+                $anexoExistente = Anexos::where('anexo', $anexo)->first();
 
-            $anexoExistente = Anexos::where('anexo', $anexo)->first();
-
-            if (!$anexoExistente) {
-                $anexos[] = [
-                    'numeros_publicos' => $numero_publico,
-                    'anexo' => $anexo,
-                    'nombre_anexo' => $nombre_anexo,
-                    'departamento' => $departamento,
-                ];
+                if (!$anexoExistente) {
+                    $anexos[] = [
+                        'numeros_publicos' => $numero_publico,
+                        'anexo' => $anexo,
+                        'nombre_anexo' => $nombre_anexo,
+                        'departamento' => $departamento,
+                    ];
+                }
             }
+            // Inserta los datos de Anexos en la base de datos
+            Anexos::insert($anexos);
+
+            return redirect()->back()->with('success', 'Los anexos se han importado correctamente.');
         }
-
-        Anexos::insert($anexos);
-
-        return redirect()->back()->with('success', 'Los anexos se han importado correctamente.');
-    }
 }
